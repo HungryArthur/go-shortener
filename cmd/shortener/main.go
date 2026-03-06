8ea8d2995d462d4171c5057ea8544d7570003234
Ваше Имя <you@example.com>
2026-03-06 11:48:32 +0300

    add go mod, and ShortPost

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,14 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+
+	"github.com/HungryArthur/go-shortener/internal/handler"
+)
+
+func main() {
+	http.HandleFunc("/", handler.ShortPost)
+
+	http.ListenAndServe(":8080", nil)
+
+}
